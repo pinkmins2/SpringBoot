ac4c4fb29a3b4de140ddb8e5c74affdaa54cbdbd
pinkmins2 <pinkmins2@gmail.com>
2023-07-05 12:20:49 +0900

    페이지 매핑 부분 추가

---
+++ src/main/java/jina/hello/boot/spring5boot/controller/JoinController.java
@@ -0,0 +1,19 @@
+package jina.hello.boot.spring5boot.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/join")
+public class JoinController {
+    Logger logger = LogManager.getLogger(JoinController.class);
+
+    @GetMapping("agree")
+    public String agree() {
+        logger.info("join/agree 호출!!");
+        return "join/agree";
+    }
+}
