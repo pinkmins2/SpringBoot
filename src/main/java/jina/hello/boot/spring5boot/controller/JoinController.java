1da67d913f6cd9d7789a66000f355f4378387938
pinkmins2 <pinkmins2@gmail.com>
2023-07-06 15:51:07 +0900

    회원가입: 실명 확인 처리

--- src/main/java/jina/hello/boot/spring5boot/controller/JoinController.java
+++ src/main/java/jina/hello/boot/spring5boot/controller/JoinController.java
@@ -1,10 +1,14 @@
 package jina.hello.boot.spring5boot.controller;
 
+import jina.hello.boot.spring5boot.model.checkme;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/join")
@@ -21,6 +25,19 @@ public class JoinController {
     public String checkme() {
         logger.info("join/checkme 호출!!");
         return "join/checkme";
+    }
+
+    @PostMapping("/checkme")
+    public String checkmeok(checkme checkme, HttpSession sess) {
+        logger.info("join/checkmeok 호출!!");
+        // checkme에 보낸 개인정보를 세션에 저장하고
+        // joinme로 이동
+        String viewPage = "redirect:/join/checkme";
+        if (checkme.getName2() != null) {
+            sess.setAttribute("checkme", checkme);
+            viewPage = "redirect:/join/joinme";
+        }
+        return viewPage;
     }
 
     @GetMapping("joinme")
