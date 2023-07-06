8d730a9f5149051819c5edfcd12ad8a7c8a67be4
pinkmins2 <pinkmins2@gmail.com>
2023-07-06 15:50:45 +0900

    회원가입: checkme 실명 확인 정보용 클래스

---
+++ src/main/java/jina/hello/boot/spring5boot/model/checkme.java
@@ -0,0 +1,14 @@
+package jina.hello.boot.spring5boot.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class checkme {
+    private String name2;
+    private String jumin1;
+    private String jumin2;
+}
