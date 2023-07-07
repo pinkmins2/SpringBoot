c03c03f142d39207fdbaea34cbd80d33be4a70bc
pinkmins2 <pinkmins2@gmail.com>
2023-07-07 10:38:56 +0900

    회원가입: Member 클래스 추가

---
+++ src/main/java/jina/hello/boot/spring5boot/model/Member.java
@@ -0,0 +1,21 @@
+package jina.hello.boot.spring5boot.model;
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Member {
+    private String mno;
+    private String userid;
+    private String passwd;
+    private String name;
+    private String email;
+    private String zipcode;
+    private String addr1;
+    private String addr2;
+    private String phone;
+    private String regdata;
+}
