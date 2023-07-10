87fce651c24f3fa168f5d8da72aa8c15c5e28894
pinkmins2 <pinkmins2@gmail.com>
2023-07-10 15:47:20 +0900

    아이디 중복 검사: MemberService 구현

--- src/main/java/jina/hello/boot/spring5boot/service/MemberServiceImpl.java
+++ src/main/java/jina/hello/boot/spring5boot/service/MemberServiceImpl.java
@@ -59,4 +59,9 @@ public class MemberServiceImpl implements MemberService{
 
         return json;
     }
+
+    @Override
+    public int checkuid(String uid) {
+        return mdao.selectOneUserid(uid);
+    }
 }
