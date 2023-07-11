24cc8412cdb1c30bd810d0f95439b59e605134e8
pinkmins2 <pinkmins2@gmail.com>
2023-07-11 11:58:03 +0900

    로그인: MemberService selectOneMember 단위 테스트

--- src/test/java/jina/hello/boot/spring5boot/MemberServiceUnitTest.java
+++ src/test/java/jina/hello/boot/spring5boot/MemberServiceUnitTest.java
@@ -26,7 +26,7 @@ public class MemberServiceUnitTest {
     @Test
     @DisplayName("MemberService save Test")
     void saveMember() {
-        Member m = new Member(null, "","","","","","","","",null);
+        Member m = new Member(null, "","","","","","","","","",null);
         boolean result = msrv.saveMember(m);
         System.out.println(result);
         assertEquals(result, true);
@@ -38,5 +38,16 @@ public class MemberServiceUnitTest {
         List<Member> results = msrv.readMember();
         System.out.println(results);
         assertNotNull(results);
+    }
+
+    @Test
+    @DisplayName("MemberService readOneMember Test")
+    void readOneMember() {
+        Member m = new Member();
+        m.setUserid("abc123");
+        m.setPasswd("987xyz");
+        Member result = msrv.readOneMember(m);
+        System.out.println(result);
+        assertNotNull(result);
     }
 }
