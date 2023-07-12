d5dcc680dac798bf65716d86adbcd3150496f6d4
pinkmins2 <pinkmins2@gmail.com>
2023-07-12 10:17:17 +0900

    게시판: Board 클래스 생성

---
+++ src/main/java/jina/hello/boot/spring5boot/model/Board.java
@@ -0,0 +1,19 @@
+package jina.hello.boot.spring5boot.model;
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Board {
+    private String bno;
+    private String title;
+    private String userid;
+    private String regdate;
+    private String thumbs;
+    private String views;
+    private String contents;
+    private String ipaddr;
+}
