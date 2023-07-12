671b20babbec62a0be7b6de8f69b6914f797bc17
pinkmins2 <pinkmins2@gmail.com>
2023-07-12 11:42:54 +0900

    게시판: 페이징 처리를 위해 코드 수정

--- src/main/java/jina/hello/boot/spring5boot/mybatis/BoardMapper.java
+++ src/main/java/jina/hello/boot/spring5boot/mybatis/BoardMapper.java
@@ -1,8 +1,6 @@
 package jina.hello.boot.spring5boot.mybatis;
 
 import jina.hello.boot.spring5boot.model.Board;
-import jina.hello.boot.spring5boot.model.Member;
-import jina.hello.boot.spring5boot.model.Zipcode;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -10,6 +8,6 @@ import java.util.List;
 @Mapper
 public interface BoardMapper {
     int insertBoard(Board b);
-    List<Board> selectBoard();
+    List<Board> selectBoard(int stnum);
     Board selectOneBoard(String bno);
 }
