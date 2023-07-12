94640be84028137eb33722063d9130b9101a5e23
pinkmins2 <pinkmins2@gmail.com>
2023-07-12 15:44:32 +0900

    게시판: view 버튼 이벤트 처리

--- src/main/resources/static/js/project.js
+++ src/main/resources/static/js/project.js
@@ -221,3 +221,30 @@ let lgoutbtn = document.querySelector("#lgoutbtn");
 lgoutbtn?.addEventListener('click', ()=> {
     location.href = '/join/logout';
 });
+
+
+// board view
+let newbdbtn = document.querySelector("#newbdbtn")
+newbdbtn?.addEventListener('click', () => {
+    location.href = '/board/write';
+});
+
+let modbdbtn = document.querySelector("#modbdbtn")
+modbdbtn?.addEventListener('click', () => {
+    if(confirm("정말 수정하시겠어요?")) {
+        alert('아직 미지원 기능입니다!!');
+    }
+});
+
+let rmvbdbtn = document.querySelector("#rmvbdbtn")
+rmvbdbtn?.addEventListener('click', () => {
+    if(confirm("정말 삭제하시겠어요?")) {
+        alert('아직 미지원 기능입니다!!');
+    }
+});
+
+let lstbdbtn = document.querySelector("#lstbdbtn")
+lstbdbtn?.addEventListener('click', () => {
+    location.href = '/board/list/1';
+});
+
