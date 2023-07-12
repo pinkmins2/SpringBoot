7588ee6f1509f248ef9ca4e080793edb8d96e282
pinkmins2 <pinkmins2@gmail.com>
2023-07-12 10:28:36 +0900

    게시판: BoardDao 단위 테스트

---
+++ src/test/java/jina/hello/boot/spring5boot/board/BoardDaoUnitTest.java
@@ -0,0 +1,33 @@
+package jina.hello.boot.spring5boot.board;
+
+import jina.hello.boot.spring5boot.dao.BoardDao;
+import jina.hello.boot.spring5boot.dao.BoardDaoImpl;
+import jina.hello.boot.spring5boot.model.Board;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(BoardDaoImpl.class)
+public class BoardDaoUnitTest {
+    @Autowired
+    private BoardDao bdao;
+
+    @Test
+    @DisplayName("BoardDao select Test")
+    void selectBoard() {
+        List<Board> results = bdao.selectBoard();
+        //System.out.println(results);
+        assertNotNull(results);
+    }
+
+}
