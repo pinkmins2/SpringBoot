6a0bdab7770300a9f995d71a95dc4c1f73de8426
pinkmins2 <pinkmins2@gmail.com>
2023-07-12 11:43:57 +0900

    게시판: Board mapper 페이징 처리 단위 테스트

--- src/test/java/jina/hello/boot/spring5boot/board/BoardMapperUnitTest.java
+++ src/test/java/jina/hello/boot/spring5boot/board/BoardMapperUnitTest.java
@@ -22,7 +22,9 @@ public class BoardMapperUnitTest {
     @Test
     @DisplayName("BoardMapper select Test")
     void selectBoard() {
-        List<Board> results = boardMapper.selectBoard();
+        int cpg = 1;
+        int stnum = (cpg -1) * 25;
+        List<Board> results = boardMapper.selectBoard(stnum);
         System.out.println(results);
         assertNotNull(results);
     }
