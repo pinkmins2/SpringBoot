18099305259c7efeacdb3154f2a82caf69f0d455
pinkmins2 <pinkmins2@gmail.com>
2023-07-13 16:06:31 +0900

    게시판: BoardMapper 검색 기능을 위한 코드 추가

--- src/main/java/jina/hello/boot/spring5boot/mybatis/BoardMapper.java
+++ src/main/java/jina/hello/boot/spring5boot/mybatis/BoardMapper.java
@@ -4,6 +4,7 @@ import jina.hello.boot.spring5boot.model.Board;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface BoardMapper {
@@ -12,4 +13,5 @@ public interface BoardMapper {
     Board selectOneBoard(String bno);
     int updateViewBoard(String bno);
     int selectCountBoard();
+    List<Board> selectFindBoard(Map<String, Object> params);
 }
