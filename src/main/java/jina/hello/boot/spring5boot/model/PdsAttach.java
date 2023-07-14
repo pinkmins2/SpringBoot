b40288b9c6b0aafab46244ae1fdb84c83829622e
pinkmins2 <pinkmins2@gmail.com>
2023-07-14 16:05:26 +0900

    자료실: PdsAttach 클래스 추가

---
+++ src/main/java/jina/hello/boot/spring5boot/model/PdsAttach.java
@@ -0,0 +1,17 @@
+package jina.hello.boot.spring5boot.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class PdsAttach {
+    private String pano;
+    private String pno;
+    private String fname;
+    private String ftype;
+    private String fsize;
+    private String fdown;
+}
