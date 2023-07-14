2e8bf5cfe48fca3eba71638a3aa9b5d80c06c29e
pinkmins2 <pinkmins2@gmail.com>
2023-07-14 16:09:16 +0900

    자료실: insertPds, lastPdsId 단위 테스트

---
+++ src/test/java/jina/hello/boot/spring5boot/pds/PdsMapperUnitTest.java
@@ -0,0 +1,40 @@
+package jina.hello.boot.spring5boot.pds;
+
+import jina.hello.boot.spring5boot.model.Board;
+import jina.hello.boot.spring5boot.model.Pds;
+import jina.hello.boot.spring5boot.mybatis.PdsMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class PdsMapperUnitTest {
+    @Autowired
+    private PdsMapper pdsMapper;
+
+    @Test
+    @DisplayName("PdsMapper lastId Test")
+    @Transactional
+    void lastIdPds() {
+        Pds p = new Pds();
+        p.setUserid("abc123");
+        p.setTitle("t");
+        p.setContents("c");
+        p.setIpaddr("127.0.0.1");
+        pdsMapper.insertPds(p);
+        int result = pdsMapper.lastPdsPno();
+        //System.out.println(result);
+        assertNotNull(result);
+    }
+}
