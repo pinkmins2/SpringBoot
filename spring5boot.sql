02213fcada04e1e42ec81ff7cf563015e4e5536e
pinkmins2 <pinkmins2@gmail.com>
2023-07-18 13:33:55 +0900

    자료실: 댓글 - pdsComments 테이블 작성

--- spring5boot.sql
+++ spring5boot.sql
@@ -93,3 +93,43 @@ select * from pds p join pdsattach pa using(pno)
 where p.pno = '17';
 
 select * from ppa where pno='17';
+
+
+-- comments
+create table pdscomments (
+    cno         int             auto_increment,
+    comments    mediumtext      not null,
+    userid      varchar(18)     not null,
+    regdate     datetime        default current_timestamp,
+    pno         int             not null,
+    ref         int             not null,
+    primary key (cno)
+);
+
+alter table pdscomments
+    add constraint fkrefcno
+        foreign key (ref) references pdscomments(cno);
+
+alter table pdscomments
+    drop constraint fkpnopno;
+
+alter table pdscomments
+    add constraint fkpnopno
+        foreign key (pno) references pds(pno);
+
+insert into pdscomments (userid, comments, ref, pno)
+values ('abc123','댓글1','1','17');
+
+insert into pdscomments (userid, comments, ref, pno)
+values ('abc123','댓글2','2','17');
+
+insert into pdscomments (userid, comments, ref, pno)
+values ('abc123','대댓글1','2','17');
+
+insert into pdscomments (userid, comments, ref, pno)
+values ('abc123','댓글3','4','17');
+
+select * from pdscomments;
+select * from pdscomments order by ref;
+select * from pdscomments where pno = 17 order by ref;
+
