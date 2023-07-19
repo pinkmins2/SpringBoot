f1cad48f7de1437921398b3d4b4bb9ff5bfa4252
pinkmins2 <pinkmins2@gmail.com>
2023-07-19 14:04:06 +0900

    자료실: 댓글 - view에서 댓글/대댓글 출력 단위 테스트

--- src/test/java/jina/hello/boot/spring5boot/pds/PdsControllerUnitTest.java
+++ src/test/java/jina/hello/boot/spring5boot/pds/PdsControllerUnitTest.java
@@ -50,4 +50,12 @@ public class PdsControllerUnitTest {
                 .andExpect(status().is(200))
                 .andDo(print());
     }
+
+    @Test
+    @DisplayName("PdsController view Test")
+    void view() throws Exception {
+        mvc.perform(get("/pds/view/17"))
+                .andExpect(status().is(200))
+                .andDo(print());
+    }
 }
