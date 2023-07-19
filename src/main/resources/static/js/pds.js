48008cd79a9d77d99e7bf45904ad2df36191a0b6
pinkmins2 <pinkmins2@gmail.com>
2023-07-19 14:50:24 +0900

    자료실: 댓글 - 댓글/대댓글 작성 코드 구현

--- src/main/resources/static/js/pds.js
+++ src/main/resources/static/js/pds.js
@@ -57,3 +57,39 @@ findbtn?.addEventListener('click', () => {
         location.href = `/pds/find/${findtype.value}/${findkey.value}/1`;
     }
 });
+
+// pds comment
+let newcmtbtn = document.querySelector("#newcmtbtn")
+newcmtbtn?.addEventListener('click', ()=> {
+   let frm = document.forms.cmtfrm;
+   if(frm.userid.value === '') alert('로그인 하세요!!');
+   else if(frm.pno.value === '') alert('로그인 하세요!!');
+   else if(frm.comments.value === '') alert('댓글을 작성하세요!!');
+   else{
+       frm.method = 'post';
+       frm.action = '/pds/cmt/write';
+       frm.submit();
+   }
+});
+
+// pds reply
+let modal = null;
+const refno = document.querySelector("#ref");
+const showReply = (ref) => {
+    refno.value = ref;    // 댓댓글을 작성할 댓글의 댓글번호(cno)를 알아냄
+    modal = new bootstrap.Modal(replyModal, {});
+    modal.show();
+};
+
+const replybtn = document.querySelector("#replybtn");
+const frm = document.querySelector("#replyfrm")
+replybtn?.addEventListener('click', () => {
+    if(frm.comments.value === '') alert('대댓글을 작성하세요!!');
+    else if(frm.ref.value === '') alert('댓글 번호가 없어요');
+    else if(frm.pno.value === '') alert('본문글 번호가 없어요');
+    else {
+        frm.method = 'post';
+        frm.action = '/pds/reply/write';
+        frm.submit();
+    }
+});
