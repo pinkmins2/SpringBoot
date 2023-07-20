1a340dc64005a2f29c1d756a863e49bf1d4ae568
pinkmins2 <pinkmins2@gmail.com>
2023-07-20 11:18:27 +0900

    갤러리: Gallery, GalAttach 클래스 추가

---
+++ src/main/java/jina/hello/boot/spring5boot/model/GalAttach.java
@@ -0,0 +1,15 @@
+package jina.hello.boot.spring5boot.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class GalAttach {
+    private String gano;
+    private String gno;
+    private String fname;
+    private String fsize;
+}
