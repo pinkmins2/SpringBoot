336468afab3a601d0c8040309f5d16c96b79e823
pinkmins2 <pinkmins2@gmail.com>
2023-07-21 10:52:34 +0900

    갤러리: gallery, galattach - 테이블, 제약조건 생성 / fname, fsize 컬럼 사이즈 증가

--- spring5boot.sql
+++ spring5boot.sql
@@ -133,3 +133,34 @@ select * from pdscomments;
 select * from pdscomments order by ref;
 select * from pdscomments where pno = 17 order by ref;
 
+
+-- gallery
+create table gallery(
+    gno         int             auto_increment,
+    title       varchar(100)    not null,
+    userid      varchar(18)     not null,
+    regdate     datetime        default current_timestamp,
+    thumbs      int             default 0,
+    views       int             default 0,
+    contents    text            not null,
+    ipaddr      varchar(15)     not null,
+    primary key (gno)
+);
+
+alter table gallery
+    add constraint fkguid
+        foreign key (userid) references member2(userid);
+
+-- galattach
+create table galattach(
+                          gano        int             auto_increment,
+                          gno         int             not null,       -- 게시글 번호
+                          fname       varchar(1024)    not null,       -- uuid 포함
+                          fsize       varchar(256)      not null,
+                          primary key (gano)
+);
+
+alter table galattach
+    add constraint fkgno
+        foreign key (gno) references gallery(gno);
+
